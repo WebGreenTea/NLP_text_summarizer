c47c2e9160e0cb3e1b81937bd18a51a4fad80b91
WebGreenTea <52134599+WebGreenTea@users.noreply.github.com>
2022-11-02 21:44:54 +0700

    Create cuda_check.py

---
+++ cuda_check.py
@@ -0,0 +1,6 @@
+# import torch
+# import tensorflow as tf
+# tf.config.list_physical_devices('GPU')
+# print(torch.__version__)
+# print(torch.cuda.is_available())
+# print(tf.test.is_gpu_available())
