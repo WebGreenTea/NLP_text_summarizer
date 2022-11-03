4bddccfa56051d99dce5a2cf8744672b398d5b2c
WebGreenTea <52134599+WebGreenTea@users.noreply.github.com>
2022-11-04 01:06:36 +0700

    update check

--- cuda_check.py
+++ cuda_check.py
@@ -1,6 +1,4 @@
-# import torch
-# import tensorflow as tf
-# tf.config.list_physical_devices('GPU')
-# print(torch.__version__)
-# print(torch.cuda.is_available())
-# print(tf.test.is_gpu_available())
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())
+
